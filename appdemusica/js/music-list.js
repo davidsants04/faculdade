38aaa82a31748488b44909839bee9af65ec1d5c7
David santos <168592312+davidsants04@users.noreply.github.com>
2024-11-02 17:51:47 -0300

    Add files via upload

---
+++ appdemusica/js/music-list.js
@@ -0,0 +1,20 @@
+let allMusic = [
+    {
+      name: "SET dj boy1", // Nome da música
+      artist: "dj boy", // Nome do artista
+      img: "music-1", // Nome do arquivo da imagem associada à música (sem extensão)
+      src: "music-1" // Nome do arquivo de áudio associado à música (sem extensão)
+    },
+    {
+      name: "SET dj boy2", // Nome da música
+      artist: "dj boy", // Nome do artista
+      img: "music-2", // Nome do arquivo da imagem associada à música (sem extensão)
+      src: "music-2" // Nome do arquivo de áudio associado à música (sem extensão)
+    },
+    {
+      name: "SET dj boy3", // Nome da música
+      artist: "dj boy", // Nome do artista
+      img: "music-3", // Nome do arquivo da imagem associada à música (sem extensão)
+      src: "music-3" // Nome do arquivo de áudio associado à música (sem extensão)
+    },
+];
